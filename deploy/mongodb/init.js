a04de36d31d3c76905ffbb124270fbd1acca2914
maxuefengdev <m17793873123@163.com>
2024-05-22 11:38:22 +0800

    fix(修复mongodb数据恢复步骤)

--- deploy/mongodb/init.js
+++
@@ -1,3 +0,0 @@
-conn = new Mongo();
-db = conn.getDB("suiteboot-example");
-db.student.insert( { name:'jack', age:29 }  );
--- deploy/mongodb/init.sh
+++ deploy/mongodb/init.sh
@@ -6,4 +6,5 @@ done
 echo "init Replica-Set..."
 mongosh --host mongodb --eval " rs.initiate( {_id: \"myReplicaSet\", members: [  {_id: 0, host: \"mongodb:27017\"}] }) "
 mongosh --host mongodb --eval " rs.status()  "
+mongorestore --host mongodb --db=suiteboot-example --drop /mongodb/data
 
