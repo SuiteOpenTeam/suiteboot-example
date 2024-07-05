373175b6fd05b9d05f8e44f148df0b2ad827b8b3
maxuefengdev <m17793873123@163.com>
2024-07-05 16:53:02 +0800

    hitfix: testing

--- src/main/java/com/suiteboot/example/Application.java
+++ src/main/java/com/suiteboot/example/Application.java
@@ -1,6 +1,6 @@
 package com.suiteboot.example;
 
-import com.suiteopen.boot.EnableSuiteOpenBoot;
+import com.suiteopen.boot.autoconfigure.EnableSuiteOpenBoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
